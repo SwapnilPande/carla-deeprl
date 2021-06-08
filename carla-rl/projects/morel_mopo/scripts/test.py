c3689fe3361c28c54f5442818ff8da3cde674096
Swapnil Pande <swapnil.pande98@gmail.com>
2021-06-08 00:05:18 -0400

    Setup base logger and comet logger

--- carla-rl/projects/morel_mopo/scripts/test.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-
-# Setup imports for algorithm and environment
-sys.path.append(os.path.abspath(os.path.join('../../../')))
-
-import projects.morel_mopo.config.dynamics_ensemble_config
--- carla-rl/projects/morel_mopo/scripts/train_dynamics.py
+++ carla-rl/projects/morel_mopo/scripts/train_dynamics.py
@@ -4,19 +4,27 @@ import os
 # Setup imports for algorithm and environment
 sys.path.append(os.path.abspath(os.path.join('../../../')))
 
-
+from common.loggers.comet_logger import CometLogger
+from projects.morel_mopo.config.logger_config import CometLoggerConfig
 from projects.morel_mopo.config.dynamics_ensemble_config import DefaultDynamicsEnsembleConfig
 from projects.morel_mopo.algorithm.dynamics_ensemble_module import DynamicsEnsemble
 from projects.morel_mopo.algorithm.data_modules import OfflineCarlaDataModule
 
+EXPERIMENT_NAME = "first_test"
+TAGS = ["dyn_only"]
 
 def main():
+    logger_conf = CometLoggerConfig()
+    logger_conf.populate(experiment_name = EXPERIMENT_NAME, tags = TAGS)
+
+    logger = CometLogger(logger_conf)
+
     class TempDataModuleConfig():
         def __init__(self):
             self.dataset_paths = ["/zfsauton/datasets/ArgoRL/swapnilp/new_state_space"]
             self.batch_size = 512
             self.frame_stack = 2
-            self.num_workers = 5
+            self.num_workers = 1
             self.train_val_split = 0.95
 
     data_config = TempDataModuleConfig()
@@ -25,11 +33,12 @@ def main():
     dyn_config = DefaultDynamicsEnsembleConfig()
     dynamics = DynamicsEnsemble(
         data_module = data_module,
-        config = dyn_config
+        config = dyn_config,
+        logger = logger
     )
 
-
-    dynamics.train(2)
+    # Train for 500 epochs
+    dynamics.train(500)
 
 
 
