4b0676e51c62baa10a19a7bf7da3980d4377e4f7
Vivian Cheng <vccheng@gpu15.int.autonlab.org>
2021-06-11 21:46:24 -0400

    testing collate

--- carla-rl/projects/morel_mopo/scripts/train_dynamics.py
+++ carla-rl/projects/morel_mopo/scripts/train_dynamics.py
@@ -4,37 +4,37 @@ import os
 # Setup imports for algorithm and environment
 sys.path.append(os.path.abspath(os.path.join('../../../')))
 
-
+from common.loggers.comet_logger import CometLogger
+from projects.morel_mopo.config.logger_config import CometLoggerConfig
 from projects.morel_mopo.config.dynamics_ensemble_config import DefaultDynamicsEnsembleConfig
 from projects.morel_mopo.algorithm.dynamics_ensemble_module import DynamicsEnsemble
 from projects.morel_mopo.algorithm.data_modules import OfflineCarlaDataModule
+from projects.morel_mopo.algorithm.fake_env import FakeEnv
+import torch
+
+torch.multiprocessing.set_sharing_strategy('file_system') 
+EXPERIMENT_NAME = "first_test"
+TAGS = ["dyn_only"]
+
+class TempDataModuleConfig():
+    def __init__(self):
+        self.dataset_paths = ["/zfsauton/datasets/ArgoRL/swapnilp/new_state_space"]
+        self.batch_size = 512
+        self.frame_stack = 2
+        self.num_workers = 1
+        self.train_val_split = 0.95
 
 
-def main():
-    class TempDataModuleConfig():
-        def __init__(self):
-            self.dataset_paths = ["/zfsauton/datasets/ArgoRL/swapnilp/new_state_space"]
-            self.batch_size = 512
-            self.frame_stack = 2
-            self.num_workers = 5
-            self.train_val_split = 0.95
-
+if __name__ == "__main__":
+ 
     data_config = TempDataModuleConfig()
     data_module = OfflineCarlaDataModule(data_config)
 
     dyn_config = DefaultDynamicsEnsembleConfig()
     dynamics = DynamicsEnsemble(
         data_module = data_module,
-        config = dyn_config
+        config = dyn_config,
+        # logger = logger
     )
-
-
-    dynamics.train(2)
-
-
-
-
-
-if __name__ == "__main__":
-    main()
+    dynamics.train(500)
 
