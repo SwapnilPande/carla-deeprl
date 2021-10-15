1abf7737076efd2439ef7f99554818476ca541df
Swapnil Pande <swapnil.pande98@gmail.com>
2021-10-15 14:58:25 -0400

    Modified carla interface to match carla interface leaderboard API

--- carla-rl/environment/carla_interfaces/carla_interface.py
+++ carla-rl/environment/carla_interfaces/carla_interface.py
@@ -2,6 +2,7 @@ import environment.carla_interfaces.scenarios_910 as scenarios
 from environment.carla_interfaces.server import CarlaServer
 from environment.carla_interfaces import planner
 from environment.carla_interfaces.actor_manager import ActorManager910, ActorManager910_Leaderboard
+from environment import env_util as util
 from abc import ABC
 import time
 import random
@@ -72,6 +73,10 @@ class Carla910Interface():
 
         # Get traffic lights
         self.traffic_actors = self.world.get_actors().filter("*traffic_light*")
+        # Initialize state for traffic lights
+        self.traffic_light_state = {
+            'initial_dist_to_red_light' : -1
+        }
 
         self.scenario_index = 0
 
@@ -181,11 +186,17 @@ class Carla910Interface():
             self.spectator.set_transform(transform)
             world_frame = self.world.tick()
 
+        left_steer = self.actor_fleet.ego_vehicle._vehicle.get_wheel_steer_angle(carla.VehicleWheelLocation.FL_Wheel)
+        right_steer = self.actor_fleet.ego_vehicle._vehicle.get_wheel_steer_angle(carla.VehicleWheelLocation.FR_Wheel)
+        # Average steering angle between front two wheels, and normalize by dividing by 90
+        steer_angle = (left_steer + right_steer) / (2* 90)
+
         transform = self.actor_fleet.get_ego_vehicle_transform()
         self.spectator.set_transform(transform)
 
         ego_vehicle_transform = self.actor_fleet.get_ego_vehicle_transform()
         ego_vehicle_velocity = self.actor_fleet.get_ego_vehicle_velocity()
+        speed = util.get_speed_from_velocity(ego_vehicle_velocity)
 
         next_orientation, \
         self.dist_to_trajectory, \
@@ -206,7 +217,10 @@ class Carla910Interface():
             'dist_to_goal' : ego_vehicle_transform.location.distance(self.destination_transform.location),
             'ego_vehicle_location' : ego_vehicle_transform,
             'ego_vehicle_velocity' : ego_vehicle_velocity,
-            'waypoints' : self.all_waypoints
+            'waypoints' : self.all_waypoints,
+            "steer_angle" : steer_angle,
+            'speed' : speed,
+            'autopilot_action' : self.get_autopilot_action()
         }
 
         control = {
@@ -217,6 +231,9 @@ class Carla910Interface():
             "control_reverse" : False,
             "control_hand_brake" : False
         }
+
+        ep_measurements["traffic_light"] = self.get_traffic_light_states()
+        ep_measurements["obstacles"] = self.get_obstacle_states(self.next_waypoints)
 
         # Create a copy of sensor_readings and ep_measurements to return
         obs = {**sensor_readings, **ep_measurements, **control}
@@ -245,6 +262,7 @@ class Carla910Interface():
 
         ego_vehicle_transform = self.actor_fleet.get_ego_vehicle_transform()
         ego_vehicle_velocity = self.actor_fleet.get_ego_vehicle_velocity()
+        speed = util.get_speed_from_velocity(ego_vehicle_velocity)
 
         next_orientation, \
         self.dist_to_trajectory, \
@@ -264,12 +282,110 @@ class Carla910Interface():
             'ego_vehicle_velocity' : ego_vehicle_velocity,
             'location' : location,
             'waypoints' : self.all_waypoints,
-            "steer_angle" : steer_angle
+            "steer_angle" : steer_angle,
+            "speed" : speed,
+            'autopilot_action' : self.get_autopilot_action()
         }
+
+        ep_measurements["traffic_light"] = self.get_traffic_light_states()
+        ep_measurements["obstacles"] = self.get_obstacle_states(self.next_waypoints)
 
         obs = {**control, **ep_measurements, **sensor_readings}
 
         return obs
+
+    def get_traffic_light_states(self):
+        ego_vehicle = self.actor_fleet.ego_vehicle
+        traffic_actor, dist, traffic_light_orientation = ego_vehicle.find_nearest_traffic_light(self.traffic_actors)
+
+
+        if traffic_light_orientation is not None:
+            self.traffic_light_state['traffic_light_orientation'] = traffic_light_orientation
+        else:
+            self.traffic_light_state['traffic_light_orientation'] = -1
+
+        if traffic_actor is not None:
+            if traffic_actor.state == carla.TrafficLightState.Red:
+                self.traffic_light_state['red_light_dist'] = dist
+
+                if self.traffic_light_state['initial_dist_to_red_light'] == -1 \
+                    or (self.traffic_light_state['nearest_traffic_actor_id'] != -1 and traffic_actor.id != self.traffic_light_state['nearest_traffic_actor_id']):
+                    self.traffic_light_state['initial_dist_to_red_light'] = dist
+
+            else:
+                self.traffic_light_state['red_light_dist'] = -1
+                self.traffic_light_state['initial_dist_to_red_light'] = -1
+
+            self.traffic_light_state['nearest_traffic_actor_id'] = traffic_actor.id
+            self.traffic_light_state['nearest_traffic_actor_state'] = traffic_actor.state
+        else:
+            self.traffic_light_state['red_light_dist'] = -1
+            self.traffic_light_state['initial_dist_to_red_light'] = -1
+            self.traffic_light_state['nearest_traffic_actor_id'] = -1
+            self.traffic_light_state['nearest_traffic_actor_state'] = None
+
+        self.traffic_light_state['dist_to_light'] = dist
+
+        return self.traffic_light_state
+
+
+    def get_obstacle_states(self, next_waypoints):
+        obstacle_state = {}
+        obstacle_state['obstacle_visible'] = False
+        obstacle_state['obstacle_orientation'] = -1
+
+        min_obs_distance = 100000000
+        found_obstacle = False
+
+        ego_vehicle_actor = self.actor_fleet.ego_vehicle._vehicle
+        map = self.map
+
+        # try:
+        for target_vehicle in self.world.get_actors():
+            # do not account for the ego vehicle
+            if target_vehicle.id == ego_vehicle_actor.id or "vehicle" not in target_vehicle.type_id:
+                continue
+
+            # if the object is not in our lane it's not an obstacle
+            target_vehicle_waypoint = map.get_waypoint(target_vehicle.get_location())
+            d_bool, d_angle, distance = util.is_within_distance_ahead(target_vehicle.get_transform(),
+                                        ego_vehicle_actor.get_transform(),
+                                        self.config.obs_config.vehicle_proximity_threshold)
+
+            if not d_bool:
+                continue
+            else:
+                if not util.check_if_vehicle_in_same_lane(ego_vehicle_actor, target_vehicle, next_waypoints, map):
+                    continue
+
+                found_obstacle = True
+                obstacle_state['obstacle_visible'] = True
+                obstacle_state['obstacle_orientation'] = d_angle
+
+                if distance < min_obs_distance:
+                    obstacle_state['obstacle_dist'] = distance
+                    obstacle_state['obstacle_speed'] = util.get_speed_from_velocity(target_vehicle.get_velocity())
+
+                    min_obs_distance = distance
+        # except:
+        #     import ipdb; ipdb.set_trace()
+
+        if not found_obstacle:
+            obstacle_state['obstacle_dist'] = -1
+            obstacle_state['obstacle_speed'] = -1
+
+        return obstacle_state
+
+    def get_autopilot_action(self, target_speed = 1.0):
+        hazard_detected = self.get_ego_vehicle().check_for_hazard()
+
+        if hazard_detected:
+            return np.array([0,-1])
+        else:
+            waypoint = self.next_waypoints[0]
+            steer = self.actor_fleet.lateral_controller.pid_control(waypoint)
+            steer = np.clip(steer, -1, 1)
+            return np.array([steer, target_speed])
 
 
     def get_actor_list(self):
@@ -290,291 +406,3 @@ class Carla910Interface():
 
         if self.server is not None:
             self.server.close()
-
-
-
-class Carla910Interface_Leaderboard:
-    def __init__(self, config, log_dir):
-        self.config = config
-
-        # Instantiate and start server
-        # print(23)
-        self.server = CarlaServer(config)
-
-        self.client = None
-
-        self.log_dir = log_dir
-
-        time.sleep(10)
-        self.setup()
-
-    def setup(self):
-        # Start the carla server and get a client
-        self.server.start()
-        self.client = self._spawn_client()
-        print(self.client.get_available_maps())
-        self.avail_map = {name[-6:]: name for name in self.client.get_available_maps()}
-        self._set_world_and_map(self.config.scenario_config.city_name)
-        print("server_version", self.client.get_server_version())
-        # print(os.getcwd())
-        self.world_annotations = RouteParser.parse_annotations_file(
-            '../../../../leaderboard/data/all_towns_traffic_scenarios_public.json')
-        # print(self.world_annotations)
-        CarlaDataProvider.set_client(self.client)
-        CarlaDataProvider.set_traffic_manager_port(4050)
-
-
-
-
-    def _set_world_and_map(self, town_name):
-        # Get the world
-        self.curr_town = town_name
-        self.world = self.client.load_world(self.curr_town)
-        # Temporary
-        self.spectator = self.world.get_spectator()
-
-
-        # Update the settings from the config
-        settings = self.world.get_settings()
-        if(self.config.sync_mode):
-            settings.synchronous_mode = True
-        if self.config.server_fps is not None and self.config.server_fps != 0:
-            settings.fixed_delta_seconds =  1.0 / float(self.config.server_fps)
-
-        # Enable rendering
-        settings.no_rendering_mode = False
-
-        self.world.apply_settings(settings)
-
-        # Sleep to allow for settings to update
-        time.sleep(5)
-
-        # Retrieve map
-        self.map = self.world.get_map()
-
-        # Get blueprints
-        self.blueprint_library = self.world.get_blueprint_library()
-        self.spawn_points = self.world.get_map().get_spawn_points()
-
-        # Instantiate a vehicle manager to handle other actors
-        self.actor_fleet = ActorManager910_Leaderboard(self.config, self.client, self.log_dir)
-
-        # Get traffic lights
-        self.traffic_actors = self.world.get_actors().filter("*traffic_light*")
-
-        self.scenario_index = 0
-
-        CarlaDataProvider.set_world(self.world)
-
-
-    def _spawn_client(self, hostname='localhost', port_number=None):
-        #TODO switch back to getting port from server
-        port_number = self.server.server_port
-        # port_number = 2000
-        client = carla.Client(hostname, port_number)
-        # print(self.config)
-        client.set_timeout(self.config.client_timeout_seconds)
-
-        return client
-
-
-    def _set_scenario(self, unseen=False, town="Town01", index=0):
-        _upd_town = town
-
-        if self.config.scenario_config.scenarios == "challenge_train_scenario":
-            self.source_transform, self.destination_transform, self.wps_list, _upd_town = scenarios.get_leaderboard_route(
-                unseen, curr_town=self.curr_town, index=index, max_idx=self.config.scenario_config.min_num_eps_before_switch_town,
-                avail_map_list=self.avail_map.keys(), mode='train')
-        elif self.config.scenario_config.scenarios == "challenge_test_scenario":
-            self.source_transform, self.destination_transform, self.wps_list, _upd_town  = scenarios.get_leaderboard_route(
-                unseen, curr_town=self.curr_town, index=index, max_idx=self.config.scenario_config.min_num_eps_before_switch_town,
-                avail_map_list=self.avail_map.keys(), mode='test')
-        else:
-            raise ValueError("Scenarios Config not set!")
-
-        if _upd_town != town: # switch to a new town
-            self._set_world_and_map(_upd_town)
-            # self.reset()
-
-
-
-    def check_subset(self, pt):
-        for spawn_pt in self.spawn_points:
-            if pt.location.distance(spawn_pt.location)<1:
-                return True
-        return False
-
-    def reset(self, unseen=False, index=0):
-        ### Delete old actors
-        self.actor_fleet.destroy_actors()
-
-        ## Set the new scenarios
-        if self.config.scenario_config.use_scenarios:
-            self._set_scenario(unseen=unseen, index=self.scenario_index, town=self.curr_town)
-            self.scenario_index += 1
-
-        ### Spawn new actors
-        self.actor_fleet.spawn(self.source_transform, unseen)
-
-        # Tick for 15 frames to handle car initialization in air
-        for _ in range(15):
-            transform = self.actor_fleet.get_ego_vehicle_transform()
-            # self.spectator.set_transform(carla.Transform(transform.location + carla.Location(z=50), carla.Rotation(pitch=-90)))
-            world_frame = self.world.tick()
-
-
-        transform = self.actor_fleet.get_ego_vehicle_transform()
-        self.spectator.set_transform(carla.Transform(transform.location + carla.Location(z=50), carla.Rotation(pitch=-90)))
-
-        # Create a global planner to generate dense waypoints along route
-        self.global_planner = planner.GlobalPlanner()
-
-        ### Setup the global planner
-        if 'challenge' in self.config.scenario_config.scenarios:
-            # print(213, len(self.wps_list))
-            _, self.route, self._global_plan_world_coord = interpolate_trajectory(self.world, self.wps_list)
-            CarlaDataProvider.set_ego_vehicle_route(convert_transform_to_location(self.route))
-            # print(222, len(self._global_plan_world_coord), self._global_plan_world_coord[0])
-            self.dense_waypoints = self._global_plan_world_coord
-        else:
-            self.dense_waypoints  = self.global_planner.trace_route(self.map,
-                                    self.source_transform, self.destination_transform)
-            # print(self.dense_waypoints)
-
-        self.global_planner.set_global_plan(self.dense_waypoints)
-
-        potential_scenarios_definitions, _ = RouteParser.scan_route_for_scenarios(
-            self.curr_town, self.route, self.world_annotations)
-
-        # Sample the scenarios to be used for this route instance.
-        self.sampled_scenarios_definitions = scenario_sampling(potential_scenarios_definitions)
-        # print(236, self.sampled_scenarios_definitions)
-        self.scenarios = build_scenario_instances(self.world, self.actor_fleet.vehicle_actor, self.sampled_scenarios_definitions, debug_mode=0)
-        # print(244, self.scenarios)
-        self.running = Trigger(self.world, self.actor_fleet.vehicle_actor, self.route, self.scenarios, debug_mode=0)
-
-        ego_vehicle_transform = self.actor_fleet.get_ego_vehicle_transform()
-        ego_vehicle_velocity = self.actor_fleet.get_ego_vehicle_velocity()
-
-        next_orientation, \
-        self.dist_to_trajectory, \
-        distance_to_goal_trajec, \
-        self.next_waypoints, \
-        self.next_wp_angles, \
-        self.next_wp_vectors, \
-        self.all_waypoints = self.global_planner.get_next_orientation_new(ego_vehicle_transform)
-
-        sensor_readings = self.actor_fleet.sensor_manager.get_sensor_readings(world_frame)
-
-
-        ep_measurements = {
-            'next_orientation' : next_orientation,
-            'distance_to_goal_trajec' : self.dist_to_trajectory,
-            'dist_to_trajectory' : self.dist_to_trajectory,
-            'next_waypoints' : self.next_waypoints,
-            'dist_to_goal' : ego_vehicle_transform.location.distance(self.destination_transform.location),
-            'ego_vehicle_location' : ego_vehicle_transform,
-            'ego_vehicle_velocity' : ego_vehicle_velocity,
-            'waypoints' : self.all_waypoints
-        }
-
-        control = {
-            "target_speed" : 0.0,
-            "control_steer" : 0.0,
-            "control_throttle" : 0.0,
-            "control_brake" : 0.0,
-            "control_reverse" : False,
-            "control_hand_brake" : False
-        }
-
-        # Create a copy of sensor_readings and ep_measurements to return
-        obs = {**sensor_readings, **ep_measurements, **control}
-        return obs
-
-
-    def step(self, action):
-        control = self.actor_fleet.step(action)
-
-        CarlaDataProvider.on_carla_tick()
-        world_frame = self.world.tick()
-        # print(290, self.running.scenario.scenario_tree.status)
-        self.actor_fleet.check_for_vehicle_elimination()
-        self.running.scenario.scenario_tree.tick_once()
-        # print(291, self.running.scenario.scenario_tree.status)
-        # if self.running.debug_mode == 1:
-
-        # print("\n")
-        # py_trees.display.print_ascii_tree(
-        #     self.running.scenario.scenario_tree, show_status=True)
-        # sys.stdout.flush()
-        # print("\n\n\n\n\n")
-
-        sensor_readings = self.actor_fleet.sensor_manager.get_sensor_readings(world_frame)
-        location = self.actor_fleet.ego_vehicle._vehicle.get_location()
-
-        left_steer = self.actor_fleet.ego_vehicle._vehicle.get_wheel_steer_angle(carla.VehicleWheelLocation.FL_Wheel)
-        right_steer = self.actor_fleet.ego_vehicle._vehicle.get_wheel_steer_angle(carla.VehicleWheelLocation.FR_Wheel)
-        # Average steering angle between front two wheels, and normalize by diving by 90
-        steer_angle = (left_steer + right_steer) / (2* 90)
-
-        transform = self.actor_fleet.get_ego_vehicle_transform()
-        self.spectator.set_transform(carla.Transform(transform.location + carla.Location(z=50), carla.Rotation(pitch=-90)))
-
-        sensor_readings["location"] = location
-
-        ego_vehicle_transform = self.actor_fleet.get_ego_vehicle_transform()
-        ego_vehicle_velocity = self.actor_fleet.get_ego_vehicle_velocity()
-
-        next_orientation, \
-        self.dist_to_trajectory, \
-        distance_to_goal_trajec, \
-        self.next_waypoints, \
-        self.next_wp_angles, \
-        self.next_wp_vectors, \
-        self.all_waypoints = self.global_planner.get_next_orientation_new(ego_vehicle_transform)
-
-        ep_measurements = {
-            'next_orientation' : next_orientation,
-            "next_waypoints" : self.next_waypoints,
-            'distance_to_goal_trajec' : self.dist_to_trajectory,
-            'dist_to_trajectory' : self.dist_to_trajectory,
-            'dist_to_goal' : ego_vehicle_transform.location.distance(self.destination_transform.location),
-            'ego_vehicle_location' : ego_vehicle_transform,
-            'ego_vehicle_velocity' : ego_vehicle_velocity,
-            'location' : location,
-            "waypoints" : self.all_waypoints,
-            'steer_angle': steer_angle
-        }
-
-        obs = {**control, **ep_measurements, **sensor_readings}
-
-        return obs
-
-
-    def get_actor_list(self):
-        return self.actor_fleet.actor_list
-
-    def get_ego_vehicle(self):
-        return self.actor_fleet.ego_vehicle
-
-    def get_traffic_actors(self):
-        return self.traffic_actors
-
-    def get_map(self):
-        return self.map
-
-    def destroy_all_actors(self):
-        # raise NotImplementedError()
-        pass
-
-    def close(self):
-        self.actor_fleet.destroy_actors()
-
-        if self.server is not None:
-            self.server.close()
-
-
-
-
-
-
