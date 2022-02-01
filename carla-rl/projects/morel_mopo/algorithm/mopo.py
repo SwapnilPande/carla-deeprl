f2c4e515fc31758b1b3daeb24f9b115af7ecd292
Swapnil Pande <swapnil.pande98@gmail.com>
2022-02-01 15:49:43 -0500

    Added ability to train policy with pre-trained dynamics model, roughed in functionality to collect data online periodically

--- carla-rl/projects/morel_mopo/algorithm/mopo.py
+++ carla-rl/projects/morel_mopo/algorithm/mopo.py
@@ -11,11 +11,16 @@ from environment.env import CarlaEnv
 
 # Stable baselines PPO
 from stable_baselines3.common.env_util import DummyVecEnv
-from stable_baselines3 import PPO
 
 # Environment
 from stable_baselines3.common.callbacks import EvalCallback
 from stable_baselines3.common.callbacks import CheckpointCallback
+
+from projects.morel_mopo.scripts.collect_data import DataCollector
+
+from common.loggers.comet_logger import CometLogger
+from projects.latgraph_drive.config.logger_config import ExistingCometLoggerConfig
+
 
 
 
@@ -60,6 +65,20 @@ class MOPO():
         # Construct env first to reserve GPU space
         env = CarlaEnv(config = self.eval_env_config, logger = self.logger, log_dir = self.logger.log_dir)
 
+
+        ## Construct evaluation environment to periodically test policy
+        eval_env = env.get_eval_env(eval_frequency = 100000)
+        dummy_eval_env = DummyVecEnv([lambda: eval_env])
+
+        eval_callback = EvalCallback(dummy_eval_env, best_model_save_path=os.path.join(self.logger.log_dir, "policy", "models"),
+                                    log_path=os.path.join(self.logger.log_dir, "policy"), eval_freq=100000,
+                                    deterministic=True, render=False,
+                                    n_eval_episodes=self.eval_env_config.scenario_config.num_episodes)
+
+        # Save a checkpoint every 1000 steps
+        checkpoint_callback = CheckpointCallback(save_freq=self.policy_epochs//10, save_path=os.path.join(self.logger.log_dir, "policy", "models"),
+                                                name_prefix='policy_checkpoint_')
+
         # Log MOPO hyperparameters
         self.logger.log_hyperparameters({
             "mopo/uncertainty_penalty" : self.fake_env_config.uncertainty_coeff,
@@ -68,52 +87,77 @@ class MOPO():
             "mopo/policy_weight_decay" : 0.0
         })
 
-        self.dynamics = self.dynamics_config.dynamics_model_type(
-                config = self.dynamics_config.dynamics_model_config,
-                data_module = self.data_module,
-                logger = self.logger)
+        # Setup dynamics model
+        # If we are using a pretrained dynamics model, we need to load it
+        # Else, we need to train a new dynamics model
+        if(self.dynamics_config.pretrained_dynamics_model is not None):
+            print("MOPO: Using pretrained dynamics model")
+            print(f"MOPO: Loading dynamics model {self.dynamics_config.pretrained_dynamics_model.name} from experiment {self.dynamics_config.pretrained_dynamics_model.key}")
 
-        print("MOPO: Beginning Dynamics Training")
+            # Construct a logger temporarily to load the dynamics model
+            logger_conf = ExistingCometLoggerConfig()
+            logger_conf.experiment_key = self.dynamics_config.pretrained_dynamics_model.key
+            temp_logger = CometLogger(logger_conf)
 
-        self.dynamics.train_model(self.dynamics_epochs)
+            self.dynamics = self.dynamics_config.dynamics_model_type.load(
+                        logger = temp_logger,
+                        model_name = self.dynamics_config.pretrained_dynamics_model.name,
+                        gpu = self.dynamics_config.gpu,
+                        data_config = self.dynamics_config.dataset_config)
 
-        print("MOPO: Constructing Fake Env")
+        # If we are not using a pretrained dynamics model, we need to train a new dynamics model
+        # Initialize a new one
+        else:
+            print("MOPO: Initializing new dynamics model")
+            self.dynamics = self.dynamics_config.dynamics_model_type(
+                    config = self.dynamics_config.dynamics_model_config,
+                    data_module = self.data_module,
+                    logger = self.logger)
+
+        data_collector = DataCollector()
+
+        self.num_online_loops = 1
+        self.num_online_samples = 50000
+
+        self.steps_per_loop = self.policy_epochs // self.num_online_loops
+        for i in range(self.num_online_loops):
+            print("MOPO: Beginning Dynamics Training")
+            # if(i == 0):
+            #     self.dynamics.train_model(self.dynamics_epochs)
+            # else:
+            #     self.dynamics.lr = self.dynamics_config.lr / 10
+            #     self.dynamics.train_model(25)
+
+            print("MOPO: Constructing Fake Env")
 
 
-        fake_env = self.dynamics_config.fake_env_type(self.dynamics,
-                        config = self.fake_env_config,
-                        logger = self.logger)
+            fake_env = self.dynamics_config.fake_env_type(self.dynamics,
+                            config = self.fake_env_config,
+                            logger = self.logger)
 
-        print("MOPO: Constructing Real Env for evaluation")
+            print("MOPO: Constructing Real Env for evaluation")
+
+            print("MOPO: Beginning Policy Training")
+
+            self.policy = self.policy_algo("MlpPolicy",
+                fake_env,
+                verbose=1,
+                carla_logger = self.logger,
+                device = self.dynamics.device,
+            )
+            self.policy.learn(total_timesteps=self.steps_per_loop, callback = [eval_callback, checkpoint_callback])
+
+            print("MOPO: Collecting online data")
+            experience_steps = 0
+            data_collector.collect_data(env = env,
+                           path = f"/home/scratch/swapnilp/temp_data/data_{i}",
+                           policy = self.policy,
+                           n_samples = self.num_online_samples,
+                           carla_gpu = self.dynamics.device,
+                    )
 
 
-        eval_env = env.get_eval_env(eval_frequency = 100000)
-        dummy_eval_env = DummyVecEnv([lambda: eval_env])
 
-        eval_callback = EvalCallback(dummy_eval_env, best_model_save_path=os.path.join(self.logger.log_dir, "policy", "models"),
-                                    log_path=os.path.join(self.logger.log_dir, "policy"), eval_freq=100000,
-                                    deterministic=False, render=False,
-                                    n_eval_episodes=self.eval_env_config.scenario_config.num_episodes)
-
-        # Save a checkpoint every 1000 steps
-        checkpoint_callback = CheckpointCallback(save_freq=self.policy_epochs//10, save_path=os.path.join(self.logger.log_dir, "policy", "models"),
-                                                name_prefix='policy_checkpoint_')
-
-        print("MOPO: Beginning Policy Training")
-
-        # import ipdb; ipdb.set_trace()
-        self.policy = self.policy_algo("MlpPolicy",
-            fake_env,
-            verbose=1,
-            carla_logger = self.logger,
-            device = self.dynamics.device,
-            #  policy_kwargs = {
-            #     "optimizer_kwargs" : {
-            #         "weight_decay" : 0.1
-            #     }
-            # }
-        )
-        self.policy.learn(total_timesteps=self.policy_epochs, callback = [eval_callback, checkpoint_callback])
         self.policy.save(os.path.join(self.logger.log_dir, "policy", "models", "final_policy"))
 
 
@@ -124,8 +168,8 @@ class MOPO():
         self.policy.save(save_dir)
         self.dynamics.save(save_dir)
 
-    def policy_predict(self, obs):
-        action, _ = self.policy.predict(obs)
+    def policy_predict(self, obs, deterministic = True):
+        action, _ = self.policy.predict(obs, deterministic = False)
         return action
 
     @classmethod
