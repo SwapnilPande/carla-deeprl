30d6697ea70f99e9ed320671e36e67299b4c826f
Swapnil Pande <swapnil.pande98@gmail.com>
2022-02-01 20:21:40 -0500

    Added support in morel_mopo_config for loading pretrained dynamics model

--- carla-rl/projects/morel_mopo/config/morel_mopo_config.py
+++ carla-rl/projects/morel_mopo/config/morel_mopo_config.py
@@ -7,13 +7,28 @@ from projects.morel_mopo.config import fake_env_config
 
 import stable_baselines3
 
+
+class PretrainedDynamicsModelConfig(BaseConfig):
+    def __init__(self):
+        super().__init__()
+        self.key = None
+        self.name = None
+        self.gpu = None
+
+    def populate(self, key, name, gpu):
+        self.key = key
+        self.name = name
+
 class BaseMOPOConfig(BaseConfig):
     def __init__(self):
         super().__init__()
 
         self.gpu = None
 
-        # Config for the dynamics model
+        ## Config for the dynamics model
+        # If pretrained dynamics model is not None, we will load it from comet
+        self.pretrianed_dynamics_model = None
+        # Else, we will train a new dynamics model using the dynamics_config passed
         self.dynamics_config = None
 
         self.fake_env_config = None
@@ -22,9 +37,21 @@ class BaseMOPOConfig(BaseConfig):
 
         self.policy_algorithm = None
 
-    def populate_config(self, gpu = 0, policy_algorithm = "PPO"):
+    def populate_config(self, gpu = 0, policy_algorithm = "PPO", pretrained_dynamics_model_key = None, pretrained_dynamics_model_name = None):
         self.gpu = 0
-        self.dynamics_config.populate_config(gpu = gpu)
+
+        # Setup dynamics config
+        # Use pretrained model if available. Else, use the dynamics_config passed
+        if(pretrained_dynamics_model_key is not None):
+            self.dynamics_config = NoneDefaultMLPObstaclesMOPOConfig
+            self.pretrained_dynamics_model = PretrainedDynamicsModelConfig()
+            self.pretrained_dynamics_model.populate(key = pretrained_dynamics_model_key,
+                                                    name = pretrained_dynamics_model_name)
+
+        else:
+            self.dynamics_config.populate_config(gpu = gpu)
+
+
         self.eval_env_config.carla_gpu = gpu
 
         self.policy_algorithm = getattr(stable_baselines3, policy_algorithm)
